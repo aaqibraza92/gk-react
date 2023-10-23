a9944662c8779415f161afd607dade2c81ee12f7
vishuthakur <43340165+vishuthakur@users.noreply.github.com>
2023-10-23 10:17:46 +0530

    css mobile view

--- src/pages/home/WhoWeAre.js
+++ src/pages/home/WhoWeAre.js
@@ -33,17 +33,17 @@ const WhoWeAre = () => {
                             <div className='circleAnimate position-relative'>
                             </div>
                             <div className='wrpCircle position-absolute'>
-                                <div className='mb0'>
-                                    <span className='fontlight subfont fs90 brownGradient fw500'>
+                                <div className='mb20'>
+                                    <span className='fontlight subfont fs90 mobFs35 brownGradient fw500'>
                                         4
                                     </span>
-                                    <span className='fontlight subfont fs90 brownGradient'>
+                                    <span className='fontlight subfont fs90 mobFs35 brownGradient'>
                                         K <svg style={{marginLeft: '-12px'}} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="35" height="35" viewBox="0,0,256,256">
 <g fill="#a07f50" fill-rule="evenodd" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(10.66667,10.66667)"><path d="M11,2v9h-9v2h9v9h2v-9h9v-2h-9v-9z"></path></g></g>
 </svg>
                                     </span>
                                 </div>
-                                <div className={`${activeTheme ? "colorBrown" : "colorWhite"}  text-uppercase fs35 lh45 fontlight`}>
+                                <div className={`${activeTheme ? "colorBrown" : "colorWhite"}  text-uppercase fs35  mobFs18 fontlight`}>
                                     Happy <br />
                                     Families
                                 </div>
