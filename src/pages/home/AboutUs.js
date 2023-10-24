f0b24a5052b92197d994ccd99744bc76e8376ffc
aaqibraza92 <aaqibraza92@gmail.com>
2023-10-24 22:54:05 +0530

    update

--- src/pages/home/AboutUs.js
+++ src/pages/home/AboutUs.js
@@ -2,70 +2,92 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
 import { useSelector } from "react-redux";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
+import Reveal from "react-reveal/Reveal";
+
 const AboutUs = () => {
   const activeTheme = useSelector((state) => {
-    return (
-      state && state?.persistedReducer?.theme?.dayTheme
-    );
+    return state && state?.persistedReducer?.theme?.dayTheme;
   });
   return (
-    <section className={`${activeTheme ? "bgWhite" : "bgFullBlack"} pt80 pb80`}> 
+    <section className={`${activeTheme ? "bgWhite" : "bgFullBlack"} pt80 pb80`}>
       <Container>
         <Row className="gy-5">
           <Col lg={6} md={6} xl={6}>
-            <p className={`${activeTheme ? "colorBlack" : "colorWhite"} mb0 fontlight fw400 fs40`}>
-            <Fade bottom cascade>
-            about us
-        </Fade>
+            <p
+              className={`${
+                activeTheme ? "colorBlack" : "colorWhite"
+              } mb0 fontlight fw400 fs40`}
+            >
+              <Fade bottom cascade>
+                about us
+              </Fade>
             </p>
             <h4 className="fs40 mb0 text-uppercase brownGradient">
-            <Fade bottom cascade>
-            Exclusive communities
-            </Fade>
+              <Fade bottom cascade>
+                Exclusive communities
+              </Fade>
             </h4>
             <h3 className="fs70 mb0 text-uppercase brownGradient">
-            <Fade bottom cascade>
-            For Holistic
-            </Fade>
+              <Fade bottom cascade>
+                For Holistic
+              </Fade>
             </h3>
             <h2 className="living mb20 text-uppercase brownGradient">
-            <Fade bottom cascade>
-            Livings
-            </Fade>
+              <Fade bottom cascade>
+                Livings
+              </Fade>
             </h2>
             <p className={`${activeTheme ? "colorBlack" : "colorWhite"}`}>
-            <Fade left cascade>
-              GK Builders and Developers aims to enhance the quality of life of
-              its customers by translating today’s trends of better and healthy
-              living into comfortable and pleasant homes. Through its superior
-              craftsmanship, customer-friendly policies and reliable track
-              record, the company has earned a stellar reputation as one of the
-              best real estate builders in Hyderabad.
+              <Fade left cascade>
+                GK Builders and Developers aims to enhance the quality of life
+                of its customers by translating today’s trends of better and
+                healthy living into comfortable and pleasant homes. Through its
+                superior craftsmanship, customer-friendly policies and reliable
+                track record, the company has earned a stellar reputation as one
+                of the best real estate builders in Hyderabad.
               </Fade>
             </p>
-            <p className={`${activeTheme ? "colorBlack" : "colorWhite"} mb70 mobMb30`}>
-            <Fade left cascade>
-              GK Builders and Developers continues to focus on futuristic
-              expansion and has chalked out plans to develop more luxurious
-              residential projects across Hyderabad and Secunderabad soon.
+            <p
+              className={`${
+                activeTheme ? "colorBlack" : "colorWhite"
+              } mb70 mobMb30`}
+            >
+              <Fade left cascade>
+                GK Builders and Developers continues to focus on futuristic
+                expansion and has chalked out plans to develop more luxurious
+                residential projects across Hyderabad and Secunderabad soon.
               </Fade>
             </p>
+
             <div className="mt10">
-            <Link className="btnTransparent mr12 fMedium btnMob" to="/#">
-              <span className={`${activeTheme ? "colorBlack" : "colorWhite"} mr10`}>Know More</span>
-              <img
-                src={require("../../assets/img/home/arrowBrown.png")}
-                className="img-fluid"
-                alt=""
-                style={{width: '28px'}}
-              />
-            </Link>
+              <Reveal effect="fadeInUp">
+                <Link className="btnTransparent mr12 fMedium btnMob" to="/#">
+                  <span
+                    className={`${
+                      activeTheme ? "colorBlack" : "colorWhite"
+                    } mr10`}
+                  >
+                    Know More
+                  </span>
+                  <img
+                    src={require("../../assets/img/home/arrowBrown.png")}
+                    className="img-fluid"
+                    alt=""
+                    style={{ width: "28px" }}
+                  />
+                </Link>
+              </Reveal>
             </div>
-       
           </Col>
           <Col lg={6} md={6} xl={6}>
-            <img src={require('../../assets/img/home/buildings.png')} className='img-fluid' alt="" />
+            <Fade right cascade>
+              <img
+                src={require("../../assets/img/home/buildings.png")}
+                className="img-fluid"
+                alt=""
+              />
+            </Fade>
           </Col>
         </Row>
       </Container>
