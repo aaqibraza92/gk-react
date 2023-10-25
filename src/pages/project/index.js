c93c39ea26bc4f02e13c7c7cb46b0c2c25283fa6
aaqibraza92 <aaqibraza92@gmail.com>
2023-10-26 00:46:39 +0530

    uu

--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -3,24 +3,37 @@ import { Col, Container, Row } from "reactstrap";
 
 const Project = () => {
   return (
-    <section>
-      <Container>
-        <h2 className="fs70 mb0 brownGradient text-center mb60">Projects</h2>
+    <>
+      <section>
+        <Container>
+          <h2 className="fs70 mb0 brownGradient text-center mb60">Projects</h2>
 
-        <Row className="justify-content-end">
-          <Col lg={9}>
-            <div className=" d-flex justify-content-end">
-              <div className="">
-              <div className="imgProject position-relative pt20 pb20 pe20">
-                  <img src={require('../../assets/img/project/pr1.png')} className="img-fluid" alt="" />
+          <Row className="justify-content-end">
+            <Col lg={9}>
+              <div className=" d-flex justify-content-end">
+                <div className="">
+                  <div className="imgProject position-relative pt20 pb20 pe20">
+                    <img
+                      src={require("../../assets/img/project/pr1.png")}
+                      className="img-fluid"
+                      alt=""
+                    />
                   </div>
+                </div>
               </div>
-                
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="pt100 pb100">
+        <Container>
+          <h2>
+          Looking for quality construction?
+          </h2>
+        </Container>
+      </section>
+    </>
   );
 };
 
