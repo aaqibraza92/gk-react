a4f2f710d390e01169a97c1f8de1cbe2159ec693
aaqibraza92 <aaqibraza92@gmail.com>
2023-10-26 17:35:57 +0530

    about

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -56,10 +56,10 @@ const AboutUs = () => {
             </Col>
 
             <Col lg={5} md={5} className="text-center">
-            <Fade right cascade>
-            <img src={require('../../assets/img/aboutus/VectorSmartObject.webp')} className='img-fluid' alt="" />
+              <Fade right cascade>
+                <img src={require('../../assets/img/aboutus/VectorSmartObject.webp')} className='img-fluid' alt="" />
               </Fade>
-           
+
             </Col>
           </Row>
         </div>
@@ -91,8 +91,13 @@ const AboutUs = () => {
               <Row className="align-items-center gy-4 ">
                 <Col lg={6} md={6} className="text-left aboutusnew">
                   <div className="bordernew">
-                    <h3 className="fs45 subfont">Our Vision</h3>
-                    <p className="fs17 colorLight w-85 mb-0">Our vision is to enhance the quality of living by offering the best living spaces in the twin cities of Hyderabad and Secunderabad. We want to give our customers an experience of best branded materials and create a comfortable.</p>
+                    <Fade bottom cascade>
+                      <h3 className="fs45 subfont">Our Vision</h3>
+                    </Fade>
+                    <Fade bottom cascade>
+                      <p className="fs17 colorLight w-85 mb-0">Our vision is to enhance the quality of living by offering the best living spaces in the twin cities of Hyderabad and Secunderabad. We want to give our customers an experience of best branded materials and create a comfortable.</p>
+                    </Fade>
+
 
                   </div>
                 </Col>
@@ -113,8 +118,13 @@ const AboutUs = () => {
 
                 <Col lg={6} md={6} className="text-left">
                   <div className="bordernew">
-                    <h3 className="fs45 subfont">Our Mission</h3>
-                    <p className="fs17 colorLight w-85">Our mission is to grow as a brand and become one of the leading developers in the industry. We aim to develop trust among our clientele and potential customers and create create a peerless reputation and track record.</p>
+                    <Fade bottom cascade>
+                      <h3 className="fs45 subfont">Our Mission</h3>
+                    </Fade>
+                    <Fade bottom >
+                      <p className="fs17 colorLight w-85">Our mission is to grow as a brand and become one of the leading developers in the industry. We aim to develop trust among our clientele and potential customers and create create a peerless reputation and track record.</p>
+                    </Fade>
+
 
                   </div>
                 </Col>
@@ -147,13 +157,13 @@ const AboutUs = () => {
         <div className="container-xl">
           <Row className="align-items-center gy-4  ">
             <Col lg={12} md={12} className="text-center">
-            <Fade bottom cascade>
-            <h3 className="fs60 mobFs30  text-uppercase subfont fontlight">Our Pillars</h3>
+              <Fade bottom cascade>
+                <h3 className="fs60 mobFs30  text-uppercase subfont fontlight">Our Pillars</h3>
               </Fade>
               <Fade bottom cascade>
-              <h3 className="fs30 fs16 subfont fontlight">GK Group’s Foundation</h3>
+                <h3 className="fs30 fs16 subfont fontlight">GK Group’s Foundation</h3>
               </Fade>
-            
+
               <div className="devider">
 
               </div>
@@ -164,12 +174,12 @@ const AboutUs = () => {
                 <img src={require('../../assets/img/aboutus/teamone.webp')} className='img-fluid w-75' alt="" />
               </Zoom>
               <div className="text-start mt20 border-start pl30 text-white">
-              <Fade bottom>
-              <p>
-                  Our respected founder Late Sri K Ramakrishnaiah was a retired Subedar in the Indian Army with a prolific record of service spanning 28 years. A visionary and philanthropist, he donated two acres of prime land to a trust to develop educational institutions. He also gifted 1000 square yards of land abutting the main road to a church and provided a place for Brahmakumaris to conduct their programs.
-                </p>
+                <Fade bottom>
+                  <p>
+                    Our respected founder Late Sri K Ramakrishnaiah was a retired Subedar in the Indian Army with a prolific record of service spanning 28 years. A visionary and philanthropist, he donated two acres of prime land to a trust to develop educational institutions. He also gifted 1000 square yards of land abutting the main road to a church and provided a place for Brahmakumaris to conduct their programs.
+                  </p>
                 </Fade>
-          
+
                 <b>
                   Late SRI. K Ramakrishnaian<br></br>
                   Founder
@@ -181,12 +191,17 @@ const AboutUs = () => {
                 <img src={require('../../assets/img/aboutus/teamtwo.webp')} className='img-fluid w-75' alt="" />
               </Zoom>
               <div className="text-start mt20 border-start pl30 text-white">
-                <p>
-                  A civil engineer from Osmania University Hyderabad, Mr. Rao has more than three decades of experience and involvement in construction of residential complexes. A leader by example, he takes active interest in resolving various issues concerning the development of projects. Mr. Rao also participates in deliberations with the government and other agencies for the welfare of builders’ community.</p>
-                <b>
-                  Mr.K. Hanumantha Rao<br></br>
-                  Managing Partner
-                </b>
+                <Fade bottom>
+                  <p>
+                    A civil engineer from Osmania University Hyderabad, Mr. Rao has more than three decades of experience and involvement in construction of residential complexes. A leader by example, he takes active interest in resolving various issues concerning the development of projects. Mr. Rao also participates in deliberations with the government and other agencies for the welfare of builders’ community.</p>
+                </Fade>
+                <Fade left>
+                  <b>
+                    Mr.K. Hanumantha Rao<br></br>
+                    Managing Partner
+                  </b>
+                </Fade>
+
               </div>
             </Col>
           </Row>
