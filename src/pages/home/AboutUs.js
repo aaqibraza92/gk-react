7a60daa938440d8796d427a279ffde2b144de296
aaqibraza92 <aaqibraza92@gmail.com>
2023-11-03 17:51:14 +0530

    u

--- src/pages/home/AboutUs.js
+++ src/pages/home/AboutUs.js
@@ -13,64 +13,64 @@ const AboutUs = () => {
         <Row className="gy-5">
           <Col lg={6} md={6} xl={6}>
             <p
-              className={`${
-                activeTheme ? "colorBlack" : "colorWhite"
-              } mb0 fontlight fw400 fs33`}
+              className={`${activeTheme ? "colorBlack" : "colorWhite"
+                } mb0 fontlight fw400 fs33`}
             >
               <Fade bottom cascade damping={0.5e-1}>
                 about us
               </Fade>
             </p>
-            
-            <Fade bottom  className="brownGradient">
+
+
             <h4 className="fs36 mb0 text-uppercase colorBrown">
-           
+              <Fade bottom cascade className="brownGradient" damping={0.5e-1}>
                 Exclusive communities
+              </Fade>
+
             </h4>
-            </Fade>
-            <Fade bottom cascade>
+
             <h3 className="fs60 mb0 text-uppercase colorBrown">
+              <Fade bottom cascade damping={0.5e-1}>
                 For Holistic
+              </Fade>
             </h3>
-            </Fade>
+
             <h2 className="living mb20 text-uppercase colorBrown">
               <Fade bottom cascade damping={0.5e-1}>
                 Livings
               </Fade>
             </h2>
             <Fade left >
-            <p className={`${activeTheme ? "colorBlack" : "colorWhite"} fs15`}>
-           
+              <p className={`${activeTheme ? "colorBlack" : "colorWhite"} fs15`}>
+
                 GK Builders and Developers aims to enhance the quality of life
                 of its customers by translating today’s trends of better and
                 healthy living into comfortable and pleasant homes. Through its
                 superior craftsmanship, customer-friendly policies and reliable
                 track record, the company has earned a stellar reputation as one
                 of the best real estate builders in Hyderabad.
-           
-            </p>
+
+              </p>
             </Fade>
             <Fade left>
-            <p
-              className={`${
-                activeTheme ? "colorBlack" : "colorWhite"
-              } mb55 mobMb30 fs15`}
-            >
-           
+              <p
+                className={`${activeTheme ? "colorBlack" : "colorWhite"
+                  } mb55 mobMb30 fs15`}
+              >
+
                 GK Builders and Developers continues to focus on futuristic
                 expansion and has chalked out plans to develop more luxurious
                 residential projects across Hyderabad and Secunderabad soon.
-         
-            </p>
+
+              </p>
             </Fade>
 
             <div className="mt10">
-            <Fade left>
+              <Fade left>
                 <Link className="btnTransparent mr12 fMedium btnMob button button--calypso" to="/#">
                   <span
-                    className={`${
-                      activeTheme ? "colorBlack" : "colorWhite"
-                    } mr10`}
+                    className={`${activeTheme ? "colorBlack" : "colorWhite"
+                      } mr10`}
                   >
                     Know More
                   </span>
