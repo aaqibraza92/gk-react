aa6d9b3d494411c1f641185352e35ece85f2ee0c
aaqibraza92 <aaqibraza92@gmail.com>
2023-11-05 11:52:17 +0530

    saasdd

--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -2,62 +2,66 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
-import Slide from 'react-reveal/Slide';
+import Slide from "react-reveal/Slide";
+import { Zoom } from "react-awesome-reveal";
+import { Fade } from "react-reveal";
 
 const Project = () => {
   const activeTheme = useSelector((state) => {
-    return (
-      state && state?.persistedReducer?.theme?.dayTheme
-    );
+    return state && state?.persistedReducer?.theme?.dayTheme;
   });
 
   return (
     <>
       <section className="position-relative">
-        
-            <div className="bigfont">
+        <Fade left>
+          <div className="bigfont">
             <h1>Our Sparkling Gems</h1>
-            </div>
-  
+          </div>
+        </Fade>
+
         <Container>
           <h2 className="fs70 mb0 brownGradient text-center mb70">Projects</h2>
-          
+
           <Row className="justify-content-end mb60 projectMainList">
             <Col lg={7}>
               <Link to="#">
                 <div className=" d-flex justify-content-end">
                   <div>
                     <div className="imgProject position-relative pt20 pb20 pr20 d-flex align-items-center justify-content-center content_wrp">
-                    <div className="position-relative">
-                    <div class="content-overlay"></div>
-                      <img
-                        src={require("../../assets/img/project/pr1.png")}
-                        className="img-fluid"
-                        alt=""
-                      />
-                    </div>
-                 
+                      <div className="position-relative">
+                        <div class="content-overlay"></div>
+                        <img
+                          src={require("../../assets/img/project/pr1.png")}
+                          className="img-fluid"
+                          alt=""
+                        />
+                      </div>
+
                       <div className="viewLink  position-absolute content-details fadeIn-bottom">
-                        
                         <Link to="#" className="">
                           <div className="circleLink text-center">
-                          <div className="crcWrapper">
-                          <img
-                              src={require("../../assets/img/home/arrowBlack.png")}
-                              className="img-fluid"
-                              alt=""
-                            />
-                          </div>
-                         
+                            <div className="crcWrapper">
+                              <img
+                                src={require("../../assets/img/home/arrowBlack.png")}
+                                className="img-fluid"
+                                alt=""
+                              />
+                            </div>
+
                             <div className="colorWhite textLink fs18 fw600">
-                            View Project
+                              View Project
                             </div>
                           </div>
                         </Link>
                       </div>
                     </div>
                     <div className="text-end mt10">
-                      <h3 className={`${activeTheme ? "colorBlack" : "colorWhite"}  fs40 fw500 mb0 text-uppercase`}>
+                      <h3
+                        className={`${
+                          activeTheme ? "colorBlack" : "colorWhite"
+                        }  fs40 fw500 mb0 text-uppercase`}
+                      >
                         GK's Casa Grande
                       </h3>
                       <p className="fs22 colorBrown">YAPRAL ROAD, SANIKPURI</p>
@@ -74,36 +78,40 @@ const Project = () => {
                 <div className=" d-flex">
                   <div>
                     <div className="imgProject2 position-relative pt20 pb20 pl20 d-flex align-items-center justify-content-center content_wrp">
-                    <div className="position-relative">
-                    <div class="content-overlay"></div>
-                      <img
-                        src={require("../../assets/img/project/pr2.jpg")}
-                        className="img-fluid"
-                        alt=""
-                      />
-                    </div>
-                 
-                            <div className="viewLink position-absolute content-details fadeIn-bottom">
+                      <div className="position-relative">
+                        <div class="content-overlay"></div>
+                        <img
+                          src={require("../../assets/img/project/pr2.jpg")}
+                          className="img-fluid"
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="viewLink position-absolute content-details fadeIn-bottom">
                         <Link to="#" className="">
                           <div className="circleLink text-center">
-                          <div className="crcWrapper">
-                          <img
-                              src={require("../../assets/img/home/arrowBlack.png")}
-                              className="img-fluid"
-                              alt=""
-                            />
-                          </div>
-                         
+                            <div className="crcWrapper">
+                              <img
+                                src={require("../../assets/img/home/arrowBlack.png")}
+                                className="img-fluid"
+                                alt=""
+                              />
+                            </div>
+
                             <div className="colorWhite textLink fs18 fw600">
-                            View Project
+                              View Project
                             </div>
                           </div>
                         </Link>
                       </div>
                     </div>
                     <div className="mt10">
-                      <h3 className={`${activeTheme ? "colorBlack" : "colorWhite"}  fs40 fw500 mb0 text-uppercase`}>
-                      GK's Zenith
+                      <h3
+                        className={`${
+                          activeTheme ? "colorBlack" : "colorWhite"
+                        }  fs40 fw500 mb0 text-uppercase`}
+                      >
+                        GK's Zenith
                       </h3>
                       <p className="fs22 colorBrown">YAPRAL ROAD, SANIKPURI</p>
                     </div>
@@ -115,40 +123,44 @@ const Project = () => {
 
           <Row className="justify-content-end mb60 projectMainList">
             <Col lg={7}>
-              <Link to="#" >
+              <Link to="#">
                 <div className=" d-flex justify-content-end">
                   <div>
                     <div className="imgProject position-relative pt20 pb20 pr20 d-flex align-items-center justify-content-center content_wrp">
-                    <div className="position-relative">
-                    <div class="content-overlay"></div>
-                      <img
-                        src={require("../../assets/img/project/pr3.jpg")}
-                        className="img-fluid"
-                        alt=""
-                      />
-                    </div>
-               
+                      <div className="position-relative">
+                        <div class="content-overlay"></div>
+                        <img
+                          src={require("../../assets/img/project/pr3.jpg")}
+                          className="img-fluid"
+                          alt=""
+                        />
+                      </div>
+
                       <div className="viewLink position-absolute  content-details fadeIn-bottom">
                         <Link to="#" className="">
                           <div className="circleLink text-center ">
-                          <div className="crcWrapper">
-                          <img
-                              src={require("../../assets/img/home/arrowBlack.png")}
-                              className="img-fluid"
-                              alt=""
-                            />
-                          </div>
-                         
+                            <div className="crcWrapper">
+                              <img
+                                src={require("../../assets/img/home/arrowBlack.png")}
+                                className="img-fluid"
+                                alt=""
+                              />
+                            </div>
+
                             <div className="colorWhite textLink fs18 fw600">
-                            View Project
+                              View Project
                             </div>
                           </div>
                         </Link>
                       </div>
                     </div>
                     <div className="text-end mt10">
-                      <h3 className={`${activeTheme ? "colorBlack" : "colorWhite"}  fs40 fw500 mb0 text-uppercase`}>
-                      GK's Alam Villas
+                      <h3
+                        className={`${
+                          activeTheme ? "colorBlack" : "colorWhite"
+                        }  fs40 fw500 mb0 text-uppercase`}
+                      >
+                        GK's Alam Villas
                       </h3>
                       <p className="fs22 colorBrown">YAPRAL ROAD, SANIKPURI</p>
                     </div>
@@ -164,36 +176,40 @@ const Project = () => {
                 <div className=" d-flex">
                   <div>
                     <div className="imgProject2 position-relative pt20 pb20 pl20 d-flex align-items-center justify-content-center content_wrp">
-                    <div className="position-relative">
-                    <div class="content-overlay"></div>
-                      <img
-                        src={require("../../assets/img/project/pr4.jpg")}
-                        className="img-fluid"
-                        alt=""
-                      />
-                    </div>
-                
-                            <div className="viewLink position-absolute  content-details fadeIn-bottom">
+                      <div className="position-relative">
+                        <div class="content-overlay"></div>
+                        <img
+                          src={require("../../assets/img/project/pr4.jpg")}
+                          className="img-fluid"
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="viewLink position-absolute  content-details fadeIn-bottom">
                         <Link to="#" className="">
                           <div className="circleLink text-center">
-                          <div className="crcWrapper">
-                          <img
-                              src={require("../../assets/img/home/arrowBlack.png")}
-                              className="img-fluid"
-                              alt=""
-                            />
-                          </div>
-                         
+                            <div className="crcWrapper">
+                              <img
+                                src={require("../../assets/img/home/arrowBlack.png")}
+                                className="img-fluid"
+                                alt=""
+                              />
+                            </div>
+
                             <div className="colorWhite textLink fs18 fw600">
-                            View Project
+                              View Project
                             </div>
                           </div>
                         </Link>
                       </div>
                     </div>
                     <div className="mt10">
-                      <h3 className={`${activeTheme ? "colorBlack" : "colorWhite"}  fs40 fw500 mb0 text-uppercase`}>
-                      GK's Festoon
+                      <h3
+                        className={`${
+                          activeTheme ? "colorBlack" : "colorWhite"
+                        }  fs40 fw500 mb0 text-uppercase`}
+                      >
+                        GK's Festoon
                       </h3>
                       <p className="fs22 colorBrown">YAPRAL ROAD, SANIKPURI</p>
                     </div>
@@ -205,15 +221,38 @@ const Project = () => {
         </Container>
       </section>
 
-      <section className={`${activeTheme ? "bgGrey" : "bgBrown"} pt100 pb100 text-center  mt50`}>
+      <section
+        className={`${
+          activeTheme ? "bgGrey" : "bgBrown"
+        } pt100 pb100 text-center  mt50`}
+      >
         <Container>
-          <h2 className={`${activeTheme ? "colorBlack" : "colorWhite"} fs50 mb55`}>Looking for quality construction?</h2>
+          <h2
+            className={`${activeTheme ? "colorBlack" : "colorWhite"} fs50 mb55`}
+          >
+            Looking for quality construction?
+          </h2>
           <Link
-                    className={`${activeTheme ? "bgBlack" : "bgBlack"} btnTheme mr12 fMedium btnMob button button--calypso`}
-                    to="/#"
-                  >
-                   <span className={`${activeTheme ? "brownGradient" : "colorWhite"} mr5`}>Know More</span> <img src={activeTheme ? require('../../assets/img/home/arrDiagonal.png') : require('../../assets/img/home/arrDiagonal.png')} className="img-fluid" alt="" />
-                  </Link>
+            className={`${
+              activeTheme ? "bgBlack" : "bgBlack"
+            } btnTheme mr12 fMedium btnMob button button--calypso`}
+            to="/#"
+          >
+            <span
+              className={`${activeTheme ? "brownGradient" : "colorWhite"} mr5`}
+            >
+              Know More
+            </span>{" "}
+            <img
+              src={
+                activeTheme
+                  ? require("../../assets/img/home/arrDiagonal.png")
+                  : require("../../assets/img/home/arrDiagonal.png")
+              }
+              className="img-fluid"
+              alt=""
+            />
+          </Link>
         </Container>
       </section>
     </>
