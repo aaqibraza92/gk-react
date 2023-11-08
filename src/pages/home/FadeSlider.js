208f30e56c4af8706241953a71241fc03f1d076e
aaqibraza92 <aaqibraza92@gmail.com>
2023-11-08 16:09:32 +0530

    update

--- src/pages/home/FadeSlider.js
+++ src/pages/home/FadeSlider.js
@@ -1,38 +1,50 @@
-import React, { useState, useEffect } from 'react';
-
-const images = [
-    'https://images.pexels.com/photos/4676396/pexels-photo-4676396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/9992332/pexels-photo-9992332.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/9992330/pexels-photo-9992330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    // Add more image URLs as needed
-  ];
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { EffectFade, Navigation, Pagination } from 'swiper';
+import { Container } from 'reactstrap';
 
 const FadeSlider = () => {
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentImageIndex((prevIndex) =>
-          prevIndex === images.length - 1 ? 0 : prevIndex + 1
-        );
-      }, 3000); // Change image every 3 seconds
-  
-      return () => clearInterval(interval);
-    }, []);
-    
   return (
-    <div className="fade-slider">
-        {
-            images.map((image, index)=>(
-                <img
-                key={index}
-                src={image}
-                alt={`Slide ${index + 1}`}
-                className={`img-fluid slider-image ${index === currentImageIndex ? 'active' : ''}`}
-              />
-            ))
-        }
-    </div>
+    <Container>
+    <Swiper
+    spaceBetween={30}
+    effect={'fade'}
+    navigation={true}
+    pagination={{
+      clickable: true,
+    }}
+    modules={[EffectFade, Navigation, Pagination]}
+    className="mySwiper"
+  >
+    <SwiperSlide>
+      <h2 className='text-dark'>
+        Demo
+      </h2>
+    </SwiperSlide>
+    <SwiperSlide>
+    <h2 className='text-dark'>
+        Demo1
+      </h2>
+    </SwiperSlide>
+    <SwiperSlide>
+    <h2 className='text-dark'>
+        Demo3
+      </h2>
+    </SwiperSlide>
+    <SwiperSlide>
+    <h2 className='text-dark'>
+        Demo4
+      </h2>
+    </SwiperSlide>
+  </Swiper>
+    </Container>
+
+
   )
 }
 
--- src/pages/home/WhoWeAre.js
+++ src/pages/home/WhoWeAre.js
@@ -1,46 +1,46 @@
 import React from 'react'
 import { Col, Container, Row } from 'reactstrap'
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper";
 import { useSelector } from "react-redux";
-import "swiper/css";
-import "swiper/css/autoplay";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/autoplay";
-import { Fade,Zoom } from "react-awesome-reveal";
-import { useEffect,useState } from 'react';
+import { Fade, Zoom } from "react-awesome-reveal";
+import { useEffect, useState } from 'react';
 import FadeSlider from './FadeSlider';
 
 
-const images = ["0","1","2","3"];
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { EffectFade, Autoplay } from 'swiper';
+
+
+const images = ["0", "1", "2", "3"];
 
 const WhoWeAre = () => {
 
     const [triggerHover, settriggerHover] = useState(false);
     const [slide, setSlide] = useState("slide1");
     useEffect(() => {
-        if(triggerHover){
+        if (triggerHover) {
             const timeout = setTimeout(() => {
                 setSlide("slide2");
-              }, 10000);
-              return () => clearTimeout(timeout);
+            }, 10000);
+            return () => clearTimeout(timeout);
         }
     }, [triggerHover]);
 
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     useEffect(() => {
-        if(triggerHover){
-      const interval = setInterval(() => {
-        setCurrentImageIndex((prevIndex) =>
-          prevIndex === images.length - 1 ? 0 : prevIndex + 1
-        );
-      }, 3000);
-  
-      return () => clearInterval(interval);
-    }
+        if (triggerHover) {
+            const interval = setInterval(() => {
+                setCurrentImageIndex((prevIndex) =>
+                    prevIndex === images.length - 1 ? 0 : prevIndex + 1
+                );
+            }, 3000);
+
+            return () => clearInterval(interval);
+        }
     }, [triggerHover]);
 
 
@@ -53,11 +53,6 @@ const WhoWeAre = () => {
     return (
         <section className={`${activeTheme ? "bgPink" : "bgFullBlack"} pt100 pb100 `}>
             <Container>
-            {/* {
-            images.map((val, index)=>(
-                    val===1 ? <h1>One</h1> :  val===2 ?   <h1>Two</h1> :  <h1>three</h1> 
-            ))
-        } */}
                 {/* <FadeSlider/> */}
                 <div className='text-center'>
                     <div className="">
@@ -65,9 +60,9 @@ const WhoWeAre = () => {
                             <img src={require('../../assets/img/home/three_dot.png')} className="img-fluid" alt="" />
                         </Zoom>
 
-                            <h2 className={`${activeTheme ? "colorBlack" : "colorWhite"} mb50 fs50 fontlight subfont text-center`}>
+                        <h2 className={`${activeTheme ? "colorBlack" : "colorWhite"} mb50 fs50 fontlight subfont text-center`}>
                             <Fade left cascade damping={1e-1} delay={100}>   who we are? </Fade>
-                            </h2>
+                        </h2>
 
                     </div>
                     <Row className='align-items-center gy-4'>
@@ -76,12 +71,66 @@ const WhoWeAre = () => {
                                 <div>
                                     <div className='circleMainWrp position-relative d-flex align-items-center justify-content-center h-100'>
 
-                                    {
-                                        triggerHover ?   <div className='circleAnimate position-relative'>
-                                        </div> :   <div className='circleAnimate hideRotate position-relative' onMouseOver={()=>settriggerHover(true)}>
-                                        </div>
-                                    }
                                         {
+                                            triggerHover ? <div className='circleAnimate position-relative'>
+                                            </div> : <div className='circleAnimate hideRotate position-relative' onMouseOver={() => settriggerHover(true)}>
+                                            </div>
+                                        }
+
+                                        {
+                                            triggerHover &&
+                                            <div className='wrpCircle position-absolute'>
+                                                <Swiper
+                                                    autoplay={{
+                                                        delay: 2500,
+                                                        disableOnInteraction: false,
+                                                    }}
+                                                    spaceBetween={0}
+                                                    effect={'fade'}
+                                                    navigation={false}
+                                                    pagination={{
+                                                        clickable: false,
+                                                    }}
+                                                    modules={[EffectFade, Autoplay]}
+                                                    className="mySwiper"
+                                                >
+                                                    <SwiperSlide>
+                                                        <div className='mb0 lh50'>
+                                                            <span className='fontlight subfont fs80 mobFs35 brownGradient fw500'>
+                                                                4
+                                                            </span>
+                                                            <span className='fontlight subfont fs80 mobFs35 brownGradient'>
+                                                                k <svg style={{ marginLeft: '-12px' }} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="35" height="35" viewBox="0,0,256,256">
+                                                                    <g fill="#a07f50" fill-rule="evenodd" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(10.66667,10.66667)"><path d="M11,2v9h-9v2h9v9h2v-9h9v-2h-9v-9z"></path></g></g>
+                                                                </svg>
+                                                            </span>
+                                                        </div>
+                                                        <div className={`${activeTheme ? "colorBrown" : "colorWhite"}  text-uppercase fs30 lh39 fontlight`}>
+                                                            Happy <br />
+                                                            Families
+                                                        </div>
+                                                    </SwiperSlide>
+                                                    <SwiperSlide>
+                                                        <div className='mb0 lh50'>
+                                                            <span className='fontlight subfont fs80 mobFs35 brownGradient fw500'>
+                                                                8
+                                                            </span>
+                                                            <span className='fontlight subfont fs80 mobFs35 brownGradient'>
+                                                                k <svg style={{ marginLeft: '-12px' }} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="35" height="35" viewBox="0,0,256,256">
+                                                                    <g fill="#a07f50" fill-rule="evenodd" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(10.66667,10.66667)"><path d="M11,2v9h-9v2h9v9h2v-9h9v-2h-9v-9z"></path></g></g>
+                                                                </svg>
+                                                            </span>
+                                                        </div>
+                                                        <div className={`${activeTheme ? "colorBrown" : "colorWhite"}  text-uppercase fs30 lh39 fontlight`}>
+                                                            Lorem <br />
+                                                            Ipsum
+                                                        </div>
+                                                    </SwiperSlide>
+
+                                                </Swiper>
+                                            </div>
+                                        }
+                                        {/* {
                                             triggerHover && slide==="slide1" &&
                                            
                                             <div className='wrpCircle position-absolute'>
@@ -126,8 +175,10 @@ const WhoWeAre = () => {
                                             </Fade>
                                         </div>
                                  
-                                        }
-                                     
+                                        } */}
+
+
+
                                     </div>
                                 </div>
 
