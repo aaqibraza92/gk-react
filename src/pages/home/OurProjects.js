7878982d958e07351de0c0062e77044b80f01969
aaqibraza92 <aaqibraza92@gmail.com>
2023-11-20 18:50:25 +0530

    image

--- src/pages/home/OurProjects.js
+++ src/pages/home/OurProjects.js
@@ -9,7 +9,32 @@ import "swiper/css/thumbs";
 import { FreeMode, Navigation, Thumbs } from "swiper";
 import { Container } from "reactstrap";
 import { useSelector } from "react-redux";
-import { Fade,Zoom } from "react-awesome-reveal";
+import { Fade, Zoom } from "react-awesome-reveal";
+
+let bigImage = [{
+  dayImage: require("../../assets/img/home/home-porfolio/almvilladay.webp"),
+  nightImage: require("../../assets/img/home/home-porfolio/nightviewalam.webp"),
+},
+{
+  dayImage: require("../../assets/img/home/home-porfolio/casaday.webp"),
+  nightImage: require("../../assets/img/home/home-porfolio/casanight.webp"),
+},
+{
+  dayImage: require("../../assets/img/home/home-porfolio/festoonday.webp"),
+  nightImage: require("../../assets/img/home/home-porfolio/festoonnight.webp"),
+},
+{
+  dayImage: require("../../assets/img/home/home-porfolio/suryaday.webp"),
+  nightImage: require("../../assets/img/home/home-porfolio/suryanight.webp"),
+},
+{
+  dayImage: require("../../assets/img/home/home-porfolio/triadday.webp"),
+  nightImage: require("../../assets/img/home/home-porfolio/traidnight.webp"),
+},
+{
+  dayImage: require("../../assets/img/home/home-porfolio/zenithday.webp"),
+  nightImage: require("../../assets/img/home/home-porfolio/zenithdaynight.webp"),
+}]
 
 const OurProjects = () => {
   const activeTheme = useSelector((state) => {
@@ -26,15 +51,13 @@ const OurProjects = () => {
         modules={[FreeMode, Navigation, Thumbs]}
         className="mySwiper2 mainProSl"
       >
-        {Array(6)
-          .fill()
-          .map((e, i) => (
+        {bigImage?.map((e, i) => (
             <SwiperSlide key={i}>
               <img
                 src={
                   activeTheme
-                    ? require("../../assets/img/home/pr_c.jpg")
-                    : require("../../assets/img/home/pr_gr.jpg")
+                    ? e?.dayImage
+                    : e?.nightImage
                 }
                 className="img-fluid"
                 alt=""
@@ -47,19 +70,19 @@ const OurProjects = () => {
           <div className="d-flex align-items-end justify-content-end w-100 h-100">
             <div className="h-100 d-flex flex-column justify-content-between w-100">
               <div className="proMaintitle">
-              <Zoom left>
-                <img
-                  src={require("../../assets/img/home/three_dot.png")}
-                  className="img-fluid"
-                  alt=""
-                />
+                <Zoom left>
+                  <img
+                    src={require("../../assets/img/home/three_dot.png")}
+                    className="img-fluid"
+                    alt=""
+                  />
                 </Zoom>
                 <Zoom left cascade damping={1e-1} delay={100}>
-                <h2 className="colorWhite fs50 fontlight subfont">
-                  ongoing projects
-                </h2>
+                  <h2 className="colorWhite fs50 fontlight subfont">
+                    ongoing projects
+                  </h2>
                 </Zoom>
-             
+
               </div>
 
               <Swiper
@@ -73,42 +96,42 @@ const OurProjects = () => {
               >
                 <SwiperSlide>
                   <img
-                    src={require("../../assets/img/home/prj1.png")}
+                    src={require("../../assets/img/home/home-porfolio/prj1.png")}
                     className="img-fluid"
                     alt=""
                   />
                 </SwiperSlide>
                 <SwiperSlide>
                   <img
-                    src={require("../../assets/img/home/prj2.png")}
+                    src={require("../../assets/img/home/home-porfolio/prj2.png")}
                     className="img-fluid"
                     alt=""
                   />
                 </SwiperSlide>
                 <SwiperSlide>
                   <img
-                    src={require("../../assets/img/home/prj3.png")}
+                    src={require("../../assets/img/home/home-porfolio/prj3.png")}
                     className="img-fluid"
                     alt=""
                   />
                 </SwiperSlide>
                 <SwiperSlide>
                   <img
-                    src={require("../../assets/img/home/prj4.png")}
+                    src={require("../../assets/img/home/home-porfolio/prj4.png")}
                     className="img-fluid"
                     alt=""
                   />
                 </SwiperSlide>
                 <SwiperSlide>
                   <img
-                    src={require("../../assets/img/home/prj5.png")}
+                    src={require("../../assets/img/home/home-porfolio/prj5.png")}
                     className="img-fluid"
                     alt=""
                   />
                 </SwiperSlide>
                 <SwiperSlide>
                   <img
-                    src={require("../../assets/img/home/prj6.png")}
+                    src={require("../../assets/img/home/home-porfolio/prj6.png")}
                     className="img-fluid"
                     alt=""
                   />
@@ -116,15 +139,15 @@ const OurProjects = () => {
               </Swiper>
 
               <div className="titles">
-                  <h2 className="colorWhite fs70 fw500 mobFs20 text-uppercase">
-                  <Zoom className="headingFont" left cascade damping={1e-1} delay={100}>Happy Home </Zoom>
-                  </h2>
-                <Fade bottom cascade>
                 <h2 className="colorWhite fs70 fw500 mobFs20 text-uppercase">
-                  <span className="fs50 mobFs15"> <Zoom className="headingFont" left cascade damping={1e-1} delay={100}>For </Zoom></span> <Zoom className="headingFont" left cascade damping={1e-1} delay={100}>Families</Zoom>
+                  <Zoom className="headingFont" left cascade damping={1e-1} delay={100}>Happy Home </Zoom>
                 </h2>
+                <Fade bottom cascade>
+                  <h2 className="colorWhite fs70 fw500 mobFs20 text-uppercase">
+                    <span className="fs50 mobFs15"> <Zoom className="headingFont" left cascade damping={1e-1} delay={100}>For </Zoom></span> <Zoom className="headingFont" left cascade damping={1e-1} delay={100}>Families</Zoom>
+                  </h2>
                 </Fade>
-             
+
               </div>
             </div>
           </div>
