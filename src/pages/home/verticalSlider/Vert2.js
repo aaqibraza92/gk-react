1a240b497cba268823d9fd12c746c4125972b3cc
aaqibraza92 <aaqibraza92@gmail.com>
2023-12-01 18:54:34 +0530

    u

--- src/pages/home/verticalSlider/Vert2.js
+++ src/pages/home/verticalSlider/Vert2.js
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import './slider.css'
 
 // import "./styles.css";
 import { Pagination, Mousewheel, Navigation } from 'swiper';
