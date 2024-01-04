77fb4051b98fa996bf0120ccefef4fa0073b490e
vishuthakur <43340165+vishuthakur@users.noreply.github.com>
2024-01-04 19:13:26 +0530

    css thanyou

--- src/pages/thankyou/index.js
+++ src/pages/thankyou/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Thankyou = () => {
   return (
-    <div className='thankyouPage text-center d-flex align-items-center justify-content-center position-relative'   style={{ backgroundImage: `url(${require("../../assets/img/thankyou.jpg")})` }}>
+    <div className='thankyouPage  text-center d-flex align-items-center justify-content-center position-relative'   style={{ backgroundImage: `url(${require("../../assets/img/thankyou.jpg")})` }}>
       <h2 className='fs50 thankH'>
-      Thank You
+      Thank You for your request.
       </h2>
+     
     </div>
   )
 }
